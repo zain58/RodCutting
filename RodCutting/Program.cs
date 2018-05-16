0564b920008adcdead93db42214f11abe400ce4a
mqpasta <mqpasta@gmail.com>
2018-05-16 22:28:56 +0500

    added bottom-up method

--- RodCutting/Program.cs
+++ RodCutting/Program.cs
@@ -10,11 +10,12 @@ namespace RodCutting
     {
         static void Main(string[] args)
         {
-            int[] profit = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30};
-            int result = CutRod(profit, profit.Length);
-            int result2 = MemoizedCutRod(profit, profit.Length);
+            int[] profit = new int[] { 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
+            //int result = CutRod(profit, profit.Length);
+            //int result = MemoizedCutRod(profit, profit.Length);
+            int result = BottomUpCutRod(profit, profit.Length);
 
-            Console.WriteLine("Optimal Solution is:{0}", result2);
+            Console.WriteLine("Optimal Solution is:{0}", result);
         }
 
         static int CutRod(int[] profit, int n)
@@ -66,6 +67,24 @@ namespace RodCutting
             r[n - 1] = q;
 
             return q;
+        }
+
+        static int BottomUpCutRod(int[] profit, int n)
+        {
+            int[] r = new int[n + 1]; 
+            r[0] = 0;
+
+            for (int j = 1; j <= n; j++)
+            {
+                int q = int.MinValue;
+                for (int i = 1; i <= j; i++)
+                {
+                    q = Math.Max(q, profit[i-1] + r[j - i]);
+                }
+                r[j] = q;
+            }
+
+            return r[n];
         }
     }
 }
